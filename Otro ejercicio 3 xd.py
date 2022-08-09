3f6a82f1ad9e710b3256156e7ac2c16dd1761646
xSamueld <109167027+xSamueld@users.noreply.github.com>
2022-08-09 13:17:57 -0600

    Create Otro ejercicio 3 xd.py

---
+++ Otro ejercicio 3 xd.py
@@ -0,0 +1,8 @@
+#Este ejercicio lo que hace es hacer una lista de numeros de 2 en 2 del tamaño de la cantidad de numeros pedida.
+n1 =int(input('Ingrese un numero '))
+
+i = 0
+
+while i < n1:
+  print(i)
+  i += 2
