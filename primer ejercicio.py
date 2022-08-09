c0d0ba9b5b279057d8fb6e1d6165780008b4b7c2
xSamueld <109167027+xSamueld@users.noreply.github.com>
2022-08-09 13:00:00 -0600

    Create primer ejercicio.py

---
+++ primer ejercicio.py
@@ -0,0 +1,7 @@
+#Este ejercicio lo que hace es pedir un numero y si ese numero es 1000 ganaste y si no, no.
+n1=int(input("Ingrese un numero "))
+
+if n1 == 1000:
+  print("ganaste un premio")
+else:
+  print("no ganaste nada")
