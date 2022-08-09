887aeab66cdd89f46b5e55babbe3688a856b8769
xSamueld <109167027+xSamueld@users.noreply.github.com>
2022-08-09 13:21:55 -0600

    Create primer programa.py

---
+++ primer programa.py
@@ -0,0 +1,30 @@
+#Como su nombre indica es el primer ejerccio y ecplica las operaciones basicas.
+print("Hola Chepe")
+
+
+n1=5
+n2=2
+
+
+print("Esto es una suma")
+res=n1+n2
+print(res)
+
+
+print("Esto es una resta")
+res=n1-n2
+print(res)
+
+
+print("Esto es una multiplicacion")
+res=n1*n2
+print(res)
+
+
+print("Esto es una division")
+res=n1/n2
+print(res)
+
+print("Esto es un modulo")
+res=n1%n2
+print(res)
