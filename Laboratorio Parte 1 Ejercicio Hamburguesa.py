51bc9c6d3b5d8762940bdc9b9fa4c42a73791463
xSamueld <109167027+xSamueld@users.noreply.github.com>
2022-08-09 13:47:30 -0600

    Create Laboratorio Parte 1 Ejercicio Hamburguesa.py

---
+++ Laboratorio Parte 1 Ejercicio Hamburguesa.py
@@ -0,0 +1,47 @@
+#Este es un programa de un menu donde pregunta que ingredientes quiere para la hamburguesa y algunos implemenot mas.
+lista = []
+print("Menú HAmburCat")
+print("Indique Si o No, si quieres los siguientes ingredientes para su hamburguesa: ")
+
+
+print("si o no")
+c = str(input("¿Desea carne? "))
+c1 = "si"
+c2 = "no"
+
+if c == c1:
+  print("si tiene carne :)")
+  lista.append("si tiene carne")
+else:
+  print("No tiene carne :(")
+
+pep = str(input("¿Desesa una pepsi? "))
+pep1 = "si"
+pep2 = "no"
+
+if pep == pep1:
+  print("Se le agrego una pepsi :)")
+  lista.append("si tiene pepsi")
+else:
+  print("No tiene pepsi :(")
+  
+print("si""no")
+str(input("¿Desea tocino? "))
+print("si","no")
+str(input("¿Desea Lechuga? "))
+print("si","no")
+str(input("¿Desea Tomate? "))
+print("si","no")
+str(input("¿Desea Cebolla? "))
+print("si","no")
+str(input("¿Desea Mayonesa? "))
+print("si","no")
+str(input("¿Desea Mostaza? "))
+print("si","no")
+str(input("¿Desea Ketchup? "))
+print("si","no")
+
+int(input("papas "))
+int(input("aros de cebolla "))
+
+
