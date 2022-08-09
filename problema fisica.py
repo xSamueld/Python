2d96ae1a350941b639d980cbb8344b32a73eba11
xSamueld <109167027+xSamueld@users.noreply.github.com>
2022-08-09 13:25:48 -0600

    Create problema fisica.py

---
+++ problema fisica.py
@@ -0,0 +1,16 @@
+#Este ejercicio responde a un problema de fisica.
+print("Un bus avanza a 72km/h. Determina el tiempo que le tomará avanzar 200m")
+
+n1 = int(input("ingrese velocidad en km/h para pasarla a m/s "))
+
+n2 = n1/3.6
+print(n2)
+
+print("tiempo = a 200m / 20m/s")
+
+t = 200/n2
+print(t)
+
+print("El tiempo en el que avanzará el bus sera en 10 segundos")
+
+
