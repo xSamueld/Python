3771f115e60a03058de58f7ba77f369bac1e375c
xSamueld <109167027+xSamueld@users.noreply.github.com>
2022-08-09 13:29:55 -0600

    Create Medio diamante.py

---
+++ Medio diamante.py
@@ -0,0 +1,10 @@
+#Este ejercicio hace un medio diamante.
+for i in range (0, 10):
+  for j in range(0, i+1):
+      print ("*", end="")
+  print()
+
+for j in range ( 0, 10):
+  for i in range(0, i):
+    print("*", end="")
+  print()
