0ab6416062debfb3842f0bf3fbcf653592ba9092
xSamueld <109167027+xSamueld@users.noreply.github.com>
2022-08-09 12:47:29 -0600

    Create Número par o impar.py

---
+++ Número par o impar.py
@@ -0,0 +1,8 @@
+#Este programa dice si un numero es par o impar.
+n1=int(input("pedir numero"))
+res=n1%2
+print(res)
+if res== 0:
+  print("el numero que ingreso es par")
+else:
+  print("el numero que ingreso es impar")
