a5d7b43059de7045dd849d256dcbc60937723e6b
xSamueld <109167027+xSamueld@users.noreply.github.com>
2022-08-09 13:05:02 -0600

    Create Segundo ejercicio.py

---
+++ Segundo ejercicio.py
@@ -0,0 +1,11 @@
+#Este ejercicio indica si el primer valor es menor que el segundo o lo contrario.
+n1=int(input("ingrese primer numero "))
+print(n1)
+
+n2=int(input("ingrese segundo numero "))
+print(n2)
+
+if n1 < n2:
+  print("el primer valor es menor que el segundo")
+else:
+  print("el primer valor es mayor que el segundo")
