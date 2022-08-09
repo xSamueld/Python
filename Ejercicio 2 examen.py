45bc0394de18590983deb251cbebac3682ce049c
xSamueld <109167027+xSamueld@users.noreply.github.com>
2022-08-09 12:57:01 -0600

    Create Ejercicio 2 examen.py

---
+++ Ejercicio 2 examen.py
@@ -0,0 +1,6 @@
+#Este ejercicio es para elevar un numero, en este caso el primer numero es el numero al cual se tiene que elevar y el segundo el numero por el cual se sube.
+n5=int(input("pedir primer numero "))
+n6=int(input("pedir segundo numero "))
+
+res=n5**n6
+print(res)
