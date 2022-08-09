eb71681b9e6fa1a8594d5d7c8a2548953b0c04b1
xSamueld <109167027+xSamueld@users.noreply.github.com>
2022-08-09 13:35:27 -0600

    Create Area de un tereno.py

---
+++ Area de un tereno.py
@@ -0,0 +1,11 @@
+#Calcula el area del cuadrado.
+base = int(input("ingrese la base "))
+altura = int(input("ingrese altura "))
+resultado = base*altura
+print(resultado)
+
+while resultado:
+  base = int(input("ingrese la base "))
+  altura = int(input("ingrese altura "))
+  resultado = base*altura
+  print(resultado)
