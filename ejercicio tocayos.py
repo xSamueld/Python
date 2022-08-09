54f523b6c8b2c0406102276b804df88b6f7071c5
xSamueld <109167027+xSamueld@users.noreply.github.com>
2022-08-09 13:01:51 -0600

    Create ejercicio tocayos.py

---
+++ ejercicio tocayos.py
@@ -0,0 +1,10 @@
+#Este ejercicio pide dos nombres para identificar si son tocayos o no.
+n3=str(input("ingrese nombre 1"))
+
+n4=str(input("ingrese nombre 2"))
+
+if n3==n4:
+  print ("si son tocayos")
+
+else: 
+    print ("no son tocayos")
