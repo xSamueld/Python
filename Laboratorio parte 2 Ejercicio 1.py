9dd7ae5a8435326de9828127712c00a7d89001e1
xSamueld <109167027+xSamueld@users.noreply.github.com>
2022-08-09 13:42:58 -0600

    Create Laboratorio parte 2 Ejercicio 1.py

---
+++ Laboratorio parte 2 Ejercicio 1.py
@@ -0,0 +1,24 @@
+n1 = int(input("ingrese un numero ")) #se le pide a la personas que ingrese 10 numeros.
+
+n2 = int(input("ingrese un numero "))
+
+n3 = int(input("ingrese un numero "))
+
+n4 = int(input("ingrese un numero "))
+
+n5 = int(input("ingrese un numero "))
+
+n6 = int(input("ingrese un numero "))
+
+n7 = int(input("ingrese un numero "))
+
+n8 = int(input("ingrese un numero "))
+
+n9 = int(input("ingrese un numero "))
+
+n10 = int(input("ingrese un numero "))
+
+respuesta1 = n1+n10 #Para poder resolver la primera parte del ejercicio se utiliza respuesta1 como una variable, esta variable es la suma de la primera y el decimo numero solicitado, luego para mostrar el resultado se pone el print para que imprima la respuesta1.
+print(respuesta1)
+
+respuesta2 = n2+n3+n4+n5+n6+n7+n8+n9 #Para poder resolver la segunda parte del ejercicio se utiliza respuesta2 como una variable, esta variable es la suma del segundo numero al noveno numero, luego para mostrar el resultado se pone el print para que imprima la respuesta2.
